0750c543adcae07fcff265564ea544bbecb1bcdd
blessedpeacemaker <lindacheck123@gmail.com>
2026-09-13 23:00:31 +0100

    Update server.js

--- server.js
+++ server.js
@@ -27,19 +27,14 @@ app.get('/', (req, res) => {
 
 // Telegram Bot Configuration
 const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
-const TELEGRAM_CHAT_ID = process.env.TELEGRAM_CHAT_ID;
-const TELEGRAM_CHAT_ID2 = process.env.TELEGRAM_CHAT_ID2;
 const TELEGRAM_CHAT_IDS = process.env.TELEGRAM_CHAT_IDS;
 
 //Post to send 
 app.post('/send', async (req, res) => {
     const { message }  = req.body;
       try {
-            const chatIds = process.env.TELEGRAM_CHAT_IDS
-                .split(',')
-                .map(id => id.trim());
-    
-            for (const chatId of chatIds) {
+            const chatIds = TELEGRAM_CHAT_IDS.split(',').map(id => id.trim());
+                for (const chatId of chatIds) {
                 await axios.post(
                     `https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage`,
                     {
@@ -48,8 +43,7 @@ app.post('/send', async (req, res) => {
                     }
                 );
             }
-    
-            res.json({ success: true });
+                res.json({ success: true });
     
         } catch (error) {
             console.error(error.response?.data || error.message);
